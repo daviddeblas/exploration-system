87170972dcb09c9bf53a87a166aac84831fe6f5b
nico <23260358+nico14916@users.noreply.github.com>
2023-02-02 15:56:38 -0500

    reinitialisation vue.js pour test et router

--- groundstation/website/src/App.vue
+++ groundstation/website/src/App.vue
@@ -1,68 +1,16 @@
-<template>
-  <body>
-    <div class="toolbar">
-      <a class="active" href="#home">INF3995 - 101 </a>
-      <a id="tools" href="#about"></a>
-    </div>
-    <Home class= 'container'/>
-  </body>
-  
-</template>
-
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import Home from './components/Home.vue';
-
-@Options({
-  components: {
-    Home,
-  },
-})
-export default class App extends Vue {}
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
 </script>
 
-<style>
-#app {
-  margin-top: 0;
-  font-family: Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
+<template>
+  <header>
+    <div class="wrapper">
+      <nav class="toolbar">
+        <RouterLink to="/">Accueil</RouterLink>
+        <RouterLink to="/history">Historique</RouterLink>
+      </nav>
+    </div>
+  </header>
 
-body {
-  background-image: url("assets/background.jpg");
-}
-
-.container {
-  margin-top : 5px;
-  text-align: center;
-}
-
-.toolbar {
-  overflow: hidden;
-  background-color: #4f312b;
-  font-family: 'Roboto', sans-serif;
-  margin-left: 10px;
-  font-weight: bold;
-}
-
-.toolbar a {
-  float: left;
-  color: #f2f2f2;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-  font-size: 17px;
-}
-
-.toolbar a:hover {
-  background-color: #ddd;
-  color: black;
-}
-
-.toolbar a.active {
-  background-color: #a24036;
-  color: white;
-}
-</style>
+  <RouterView />
+</template>
---
+++ groundstation/website/src/views/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="home">
+    <h1 id="title">Gestion de la mission</h1>
+    <div id="buttons">
+      <button class="btn">Lancer</button>
+      <button class="btn">Identifier</button>
+      <button class="btn">Terminer</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.home {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 25px auto;
+  justify-content: center;
+  width: 80%;
+  font-family: "Roboto", sans-serif;
+  background: #df9d81;
+  padding: 25px;
+}
+#title {
+  color: #943e36;
+  font-weight: bold;
+  font-size: 50px;
+  text-align: center;
+}
+#buttons {
+  display: flex;
+  gap: 25px;
+}
+.btn {
+  background-color: #13ce66;
+  font-size: 20px;
+  font-family: "Roboto", sans-serif;
+  padding: 10px;
+  color: rgb(55, 21, 21);
+  cursor: pointer;
+}
+</style>
