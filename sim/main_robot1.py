867060e5207708271cd8c45de96eadebe200fd10
theolouesdon <65035689+theolouesdon@users.noreply.github.com>
2023-02-08 21:51:03 -0500

    Created 2 robot files to have different movements

--- sim/main_robot1.py
+++ sim/main_robot1.py
@@ -3,16 +3,16 @@ import rospy
 from geometry_msgs.msg import Twist
 MOVE_TIME_SEC = 0.01
 
-pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
-rospy.init_node('movement')
+pub = rospy.Publisher('/robot1/cmd_vel', Twist, queue_size=10)
+rospy.init_node('movement_limo1', anonymous=False)
 rate=rospy.Rate(10)
 move = Twist()
 
 def start_listener(sample):
     message = sample.payload.decode('utf-8')
     print(message)
-    move.linear.x = 0.5
-    move.linear.z = 0.2
+    move.linear.x = -0.5
+    move.linear.z = -0.5
     pub.publish(move)
 
 def identify_listener(sample):
@@ -23,6 +23,7 @@ def finish_listener(sample):
     message = sample.payload.decode('utf-8')
     print(message)
     move.linear.x = 0.0
+    move.linear.z = 0.0
     pub.publish(move)
 
 if __name__ == "__main__":
@@ -35,5 +36,8 @@ if __name__ == "__main__":
     sub3 = session.declare_subscriber('finish', finish_listener)
 
     print("Started listening")
+    print(rospy.get_namespace())
+    rospy.resolve_name("/robot2/")
+    print(rospy.get_namespace())
     while True:
         time.sleep(1)
---
+++ sim/main_robot2.py
@@ -0,0 +1,43 @@
+import zenoh, time
+import rospy
+from geometry_msgs.msg import Twist
+MOVE_TIME_SEC = 0.01
+
+pub = rospy.Publisher('/robot2/cmd_vel', Twist, queue_size=10)
+rospy.init_node('movement_limo2', anonymous=False)
+rate=rospy.Rate(10)
+move = Twist()
+
+def start_listener(sample):
+    message = sample.payload.decode('utf-8')
+    print(message)
+    move.linear.x = 0.5
+    move.linear.z = 0.5
+    pub.publish(move)
+
+def identify_listener(sample):
+    message = sample.payload.decode('utf-8')
+    print(message)
+    
+def finish_listener(sample):
+    message = sample.payload.decode('utf-8')
+    print(message)
+    move.linear.x = 0.0
+    move.linear.z = 0.0
+    pub.publish(move)
+
+if __name__ == "__main__":
+    move.linear.x = 0.0
+    move.linear.z = 0.0
+    pub.publish(move)
+    session = zenoh.open()
+    sub1 = session.declare_subscriber('start', start_listener)
+    sub2 = session.declare_subscriber('identify', identify_listener)
+    sub3 = session.declare_subscriber('finish', finish_listener)
+
+    print("Started listening")
+    print(rospy.get_namespace())
+    rospy.resolve_name("/robot2/")
+    print(rospy.get_namespace())
+    while True:
+        time.sleep(1)
