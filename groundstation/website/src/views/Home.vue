f9ccceebd724a1c70376b2e8fb80127d61afbcf2
daviddeblas <71023594+daviddeblas@users.noreply.github.com>
2023-02-21 00:28:16 -0500

    Rajout envoie données de la simulation vers le serveur web (Problème à corriger)

--- groundstation/website/src/views/Home.vue
+++ groundstation/website/src/views/Home.vue
@@ -2,6 +2,9 @@
 import { inject } from "vue";
 import { socketProvider } from "@/plugins/socket";
 import type { Socket } from "socket.io-client";
+import { ROBOT_STATUS } from "@/common/constants";
+
+let roverStatus = ROBOT_STATUS.offline; 
 
 const socket = inject(socketProvider) as Socket;
 
@@ -16,6 +19,19 @@ function identify() {
 function finish() {
   socket.emit("finish", { data: "Finir mission" });
 }
+
+socket.on("robotState", (in_mission) => {
+  console.log("in_mission: " + in_mission)
+  const robot_state = JSON.parse(in_mission);
+  console.log("robot state" + robot_state);
+  
+  if (in_mission) {
+    roverStatus = ROBOT_STATUS.in_mission;
+  } else {
+    roverStatus = ROBOT_STATUS.pending;
+  }
+});
+
 </script>
 
 <template>
@@ -26,6 +42,10 @@ function finish() {
       <button class="btn" @click="identify">Identifier</button>
       <button class="btn" @click="finish">Terminer</button>
     </div>
+    <div>
+    <span>Le rover est {{ roverStatus }} </span>
+  </div>
+
   </div>
 </template>
 
