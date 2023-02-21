3bc165cbef529092ec78053eb7b994736c310a6b
daviddeblas <71023594+daviddeblas@users.noreply.github.com>
2023-02-21 00:29:26 -0500

    envoie données de la simulation vers serveur web

--- sim/main_robot1.py
+++ sim/main_robot1.py
@@ -1,29 +1,39 @@
-import zenoh, time
+import zenoh
+import time
 import rospy
 from geometry_msgs.msg import Twist
 
 pub = rospy.Publisher('/robot1/cmd_vel', Twist, queue_size=10)
 rospy.init_node('movement_limo1', anonymous=False)
-rate=rospy.Rate(10)
+rate = rospy.Rate(10)
 move = Twist()
+in_mission = False
+
 
 def start_listener(sample):
+    global in_mission
     message = sample.payload.decode('utf-8')
     print(message)
     move.linear.x = -0.5
     move.linear.y = -0.5
     pub.publish(move)
+    in_mission = True
+
 
 def identify_listener(sample):
     message = sample.payload.decode('utf-8')
     print(message)
-    
+
+
 def finish_listener(sample):
+    global in_mission
     message = sample.payload.decode('utf-8')
     print(message)
     move.linear.x = 0.0
     move.linear.y = 0.0
     pub.publish(move)
+    in_mission = False
+
 
 if __name__ == "__main__":
     move.linear.x = 0.0
@@ -33,7 +43,7 @@ if __name__ == "__main__":
     sub1 = session.declare_subscriber('start', start_listener)
     sub2 = session.declare_subscriber('identify', identify_listener)
     sub3 = session.declare_subscriber('finish', finish_listener)
-
     print("Started listening")
     while True:
         time.sleep(1)
+        pub1 = session.declare_publisher('robotState').put(in_mission)
