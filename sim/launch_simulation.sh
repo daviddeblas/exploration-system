e463a162a0ff6234bbfb7fcab8442b06dda7cf8f
theolouesdon <65035689+theolouesdon@users.noreply.github.com>
2023-03-10 18:44:38 -0500

    Limos to spawn at least 0.4 blocks away from walls

--- sim/launch_simulation.sh
+++ sim/launch_simulation.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-(
-    cd ./src/ugv_gazebo_sim/limo/limo_gazebo_sim/worlds/
-    python3 wall_generator.py 4
-)
-x=$((((RANDOM % 101) / 10) - 5))   # Les positions possibles sont entre -5 et 5
-y=$((((RANDOM % 101) / 10) - 5))   # Les positions possibles sont entre -5 et 5
-yaw=$((RANDOM % 7))         # Environ 2pi pour l'orientation
+
+cd ./src/ugv_gazebo_sim/limo/limo_gazebo_sim/worlds/
+x_y=$(python3 wall_generator.py 3)
+
+x=$(echo $x_y | cut -d ',' -f 1)
+y=$(echo $x_y | cut -d ',' -f 2)
+yaw=$((RANDOM % 7))
 x2=$((((RANDOM % 101) / 10) - 5))
 y2=$((((RANDOM % 101) / 10) - 5))
 yaw2=$((RANDOM % 7))
--- sim/wall_generator.py
+++ sim/wall_generator.py
@@ -1,11 +1,43 @@
 import sys
 import random
+import math
+
+def is_position_valid(x, y, pose):
+    # Calculate the distance between the point and the center of the object
+    dx = x - pose[0]
+    dy = y - pose[1]
+    distance = math.sqrt(dx ** 2 + dy ** 2)
+
+    # Calculate the length of the object along the x axis with a yaw applied
+    length = 7
+    angle = pose[2]
+    length_x = abs(length * math.cos(angle))
+    length_y = abs(length * math.sin(angle))
+
+    # Calculate the coordinates of the four corners of the object
+    corners = [(pose[0] + length_x / 2, pose[1] + length_y / 2),
+               (pose[0] - length_x / 2, pose[1] - length_y / 2)]
+
+    i=0
+    j=1
+    # Calculate the equation of the line segment in the form y = mx + b
+    if corners[i][0] == corners[j][0]:
+        # Vertical line, slope is infinite
+        if x == corners[i][0] and min(corners[i][1], corners[j][1]) - 0.4 <= y <= max(corners[i][1], corners[j][1]) + 0.4:
+            return False
+    else:
+        # Non-vertical line, calculate slope and y-intercept
+        m = (corners[j][1] - corners[i][1]) / (corners[j][0] - corners[i][0])
+        b = corners[i][1] - m * corners[i][0]
+        if abs(y - (m * x + b)) < 0.4 and min(corners[i][0], corners[j][0]) <= x <= max(corners[i][0], corners[j][0]):
+            return False
+    return True
 
 def generate_random_pose():
     x = random.uniform(-4.0, 4.0)
     y = random.uniform(-4.0, 4.0)
     yaw = random.uniform(-3.14, 3.14)
-    return "%.4f %.4f 0.0001 0 0 %.4f" % (x, y, yaw)
+    return x, y, yaw
 
 # Forme de chaque model
 model_template = """
@@ -22,11 +54,13 @@ if len(sys.argv) != 2:
 else:
     num_models = int(sys.argv[1])
 
-# Generer les modèles avec des positions aléatoires
+# Generer les mod�les avec des positions al�atoires
 models = []
+poses = []
 for i in range(num_models):
     pose = generate_random_pose()
-    models.append(model_template.format(idx=i, pose=pose))
+    poses.append(pose)
+    models.append(model_template.format(idx=i, pose=("%.4f %.4f 0.0001 0 0 %.4f" % pose)))
 
 # Combine all models into a single string
 models_str = "\n".join(models)
@@ -36,7 +70,7 @@ file_path = "./personalised.world"
 with open(file_path, "r") as file:
     content = file.readlines()
 
-# Trouve la ligne où ajouter les modèle
+# Trouve la ligne o� ajouter les mod�le
 for i, line in enumerate(content):
     if """<world name="default">""" in line:
         content.insert(i + 1, models_str)
@@ -44,3 +78,15 @@ for i, line in enumerate(content):
 
 with open(file_path, "w") as f:
     f.write("".join(content))
+
+valid_pos = False    
+while not valid_pos:
+    x = random.uniform(-5.0, 5.0)
+    y = random.uniform(-5.0, 5.0)
+    valid_pos = True
+    wall_total_length = 7
+    for walls in poses:
+        if not is_position_valid(x, y, walls):
+            valid_pos = False
+            break
+print(f"{x},{y}")
