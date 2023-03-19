30e1404f4421db7a1aec800ffbf90189b63fdfcd
daviddeblas <71023594+daviddeblas@users.noreply.github.com>
2023-03-19 18:23:59 -0400

    Rajout de la logique derrière le parcours du drone en simulation

---
+++ sim/constants.py
@@ -0,0 +1,9 @@
+#Variable relatif au temps que le drone met pour effectuer un mouvement
+TIME_TO_TURN_90 = 1.57
+TIME_TO_TURN_145 = 2.355
+END_LINE_TIME = 4.16
+REPOSITION_TIME = 2.1
+CROSS_THE_MAP_TIME = 5.88
+
+#Variable relative au nombre de ligne que le drone doit parcourir
+NUMBER_OF_LINE = 11
