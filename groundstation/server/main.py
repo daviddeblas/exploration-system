f426bc26b791fac52bac5168753f2f1b37383862
nico <23260358+nico14916@users.noreply.github.com>
2023-03-21 16:38:35 -0400

    Remove unused import

--- groundstation/server/main.py
+++ groundstation/server/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
-from fastapi.middleware.cors import CORSMiddleware
 
 from sockets import sio_app
 
