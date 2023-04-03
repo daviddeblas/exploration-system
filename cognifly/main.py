26aa760cc441c35b6a62b701f2af99a79e37a700
Exaucé C. Ngay <exaucengay@gmail.com>
2023-04-03 18:22:15 -0400

    modif main_cognifly

--- cognifly/main.py
+++ cognifly/main.py
@@ -1,14 +1,17 @@
 import zenoh, time
 from cognifly import Cognifly
 
+cf = Cognifly(drone_hostname="Cognifly1.lan", gui=False)
+
 def identify_listener(sample):
-    cf = Cognifly(drone_hostname="my_drone_name.local")
-    cf.arm()
-    time.sleep(2)
-    cf.disarm()
+    global cf
+    global position
+    position = cf.get_position(sample)
 
 if __name__ == "__main__":
+    global position
     session = zenoh.open()
     sub = session.declare_subscriber('identify', identify_listener)
     while True:
         time.sleep(1)
+        print("position is: ", position )
