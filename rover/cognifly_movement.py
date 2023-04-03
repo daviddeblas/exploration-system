88d6f08e4ed59f64c9543c4dabe5b1f8f0e4a2ea
Exaucé C. Ngay <exaucengay@gmail.com>
2023-04-03 18:24:06 -0400

    identify cognifly implemented

--- rover/cognifly_movement.py
+++ rover/cognifly_movement.py
@@ -1,9 +1,12 @@
 from cognifly import Cognifly
 import time
+import zenoh
 
 MAX_VELOCITY=0.25
 MAX_YAW=1.0
 
+DRONE = "cognifly",
+ROVER = "limo",
 class MoveCognifly:
     def __init__(self) -> None:
         self.cf = Cognifly(drone_hostname="Cognifly2.lan", gui=False)
@@ -49,13 +52,35 @@ class MoveCognifly:
         self.cf.land_nonblocking()
         time.sleep(5)
         self.cf.disarm()
+
+    def identify_cognifly(self, session, limo_pos):
+        self.cf = Cognifly(drone_hostname="cognifly1")
+        if ( self.distance_calculation(limo_pos) == DRONE ) : 
+            session.declare_publisher('cogniflyId')
     
-    def identify_cognifly(self):
-        if not self.started_mission and not self.finishing_mission:
-            self.cf.arm()
-            time.sleep(2)
-            self.cf.disarm()
-    
+    def distance_calculation(self, limo_position):
+        cognifly_position = self.cf.get_position()
+        robot = {x: " ",y: " "}
+        difference = {x: 0,y: 0}
+
+        if ( cognifly_position.x > limo_position.x ) : 
+            difference.x = cognifly_position.x - limo_position.x
+            robot.x = DRONE 
+        else : 
+            difference.x = limo_position.x - cognifly_position.x
+            robot.x = ROVER
+        
+        if ( cognifly_position.y > limo_position.y) : 
+            difference.y = cognifly_position.y - limo_position.y
+            robot.y = DRONE
+        else : 
+            difference.y = limo_position.y - cognifly_position.y
+            robot.y = ROVER
+
+        if ( difference.x > difference.y) : 
+            return robot.x
+        else : return robot.y
+
     def finish_mission(self):
         self.finishing_mission = True
         self.cf.set_position_nonblocking(x=0.0, y=0.0, z=1.0, yaw=0.0,
@@ -66,3 +91,4 @@ class MoveCognifly:
         self.cf.disarm()
         self.finishing_mission = False
         self.started_mission = False
+        
