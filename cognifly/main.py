73de0b28471fc6163dbe98e7bd0a9bb79fd2420e
Exaucé C. Ngay <exaucengay@gmail.com>
2023-04-06 17:36:44 -0400

    ajout led code in main

--- cognifly/main.py
+++ cognifly/main.py
@@ -1,14 +1,33 @@
 import zenoh, time
-from cognifly import Cognifly
+import RPi.GPIO as GPIO
 
+from cognifly import Cognifly
 cf = Cognifly(drone_hostname="Cognifly1.lan", gui=False)
+session = zenoh.open()
+session.declare_subscriber ('cognifly_id', led_light)
+is_called = False
 
 def identify_listener(sample):
     global cf
     global position
     position = cf.get_position(sample)
 
+def led_light:
+    global is_called
+    if is_called: return
+    is_called = True
+
+    GPIO.output(26,GPIO.HIGH)
+    print ("LED is ON")
+    time.sleep(1)
+    GPIO.output(26,GPIO.LOW)
+    print ("LED is OFF")
+    is_called = False
+
 if __name__ == "__main__":
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    
     global position
     session = zenoh.open()
     sub = session.declare_subscriber('identify', identify_listener)
