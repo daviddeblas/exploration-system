55f44145608cb88017798dea5a779e62a8be240f
Exaucé C. Ngay <exaucengay@gmail.com>
2023-04-14 20:45:53 -0400

    implementing tests for main rover

---
+++ rover/main.test.py
@@ -0,0 +1,86 @@
+import unittest
+from unittest.mock import patch
+import main
+
+class TestMain(unittest.TestCase):
+    def setUp(self):
+        self.sample = type('Sample', (object,), {'payload': b'start exploration'})
+
+    # Test de start_listener et que le booléen soit bien modifié
+    def test_start_listener(self):
+        with patch('builtins.print') as mocked_print:
+            with patch.object(main.cognifly, 'start_mission') as mocked_cognifly_start
+                main.start_listener(self.sample)
+                mocked_print.assert_called_with('start exploration')
+                mocked_cognifly_start.assert_called_once()
+                self.assertTrue(main.start_exploration)
+
+    # Test de identify_rover 
+    # def test_identify_rover(self):
+            # main.identify_rover()
+    
+    # Test de identify_listener for rover and cognifly
+    def test_identify_listener(self):
+        with patch('identify_rover') as mocked_id_rover:
+            with patch.object(main.cognifly, 'identify_cognifly') as mocked_id_cognifly:
+                main.identify_listener(self.sample)
+                mocked_id_rover.assert_called_once()
+                mocked_id_cognifly.assert_called_once()
+        
+
+    # Test de finish_listener et que le booléen soit bien modifié
+    def test_finish_listener(self):
+        with patch('builtins.print') as mocked_print:
+            with patch.object(main.launch_exploration, 'shutdown') as mocked_shutdown:
+                main.finish_listener(self.sample)
+                mocked_print.assert_called_with('start exploration')
+                mocked_shutdown.assert_called_once()
+                self.assertFalse(main.exploration_running)
+
+    # Test de farthest_robot_trigger is cognifly
+    def farthest_robot_trigger():
+        with patch.object(main.cognifly, 'identify_cognifly') as mocked_id_cognifly:
+            with patch.object(main.cognifly, 'distance_calculation', 10 ) as mocked_cognifly_distance:
+                main.last_position = {x: 0, y: 1}
+                main.farthest_robot_trigger()
+                mocked_cognifly_distance.assert_called_once()
+                mocked_id_cognifly.assert_called_once()
+
+    # Test de farthest_robot_trigger is rover
+    def farthest_robot_trigger():
+        with patch('identify_rover') as mocked_id_rover:
+            with patch.object(main.cognifly, 'distance_calculation', 0 ) as mocked_cognifly_distance:
+                main.last_position = {x: 5, y: 5}
+                main.farthest_robot_trigger()
+                mocked_cognifly_distance.assert_called_once()
+                mocked_id_rover.assert_called_once()
+
+    # Test de p2p_trigger
+    def p2p_trigger(self):
+        main.p2p_trigger(self.sample)
+        self.assertTrue(main.activate_p2p)
+       
+    # Test que la fonction main() change bien la valeur des booléens lorsqu'ils sont prêts à démarrer l'exploration
+    @patch('main.zenoh.open')
+    def test_main(self, mocked_open):
+        main.start_exploration = True
+        main.exploration_running = False
+
+        with patch('main.time.sleep') as mocked_sleep:
+            def side_effect(*args, **kwargs):
+                if mocked_sleep.call_count >= 2:
+                    raise StopIteration
+                return None
+            mocked_sleep.side_effect = side_effect
+
+            with patch.object(main.launch_exploration, 'start') as mocked_start:
+                with self.assertRaises(StopIteration):
+                    main.main()
+                mocked_open.assert_called_once()
+                mocked_sleep.assert_called_with(1)
+                self.assertFalse(main.start_exploration)
+                self.assertTrue(main.exploration_running)
+                mocked_start.assert_called_once()
+
+if __name__ == '__main__':
+    unittest.main()
