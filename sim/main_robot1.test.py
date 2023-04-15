3fe6983908fec352b30fa5a8d45d174c4239b0b2
daviddeblas <71023594+daviddeblas@users.noreply.github.com>
2023-04-15 18:23:06 -0400

    Rajout test pour le drone et le rover dans la simulation

--- sim/main_robot1.test.py
+++ sim/main_robot1.test.py
@@ -1,48 +1,80 @@
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 import main_robot1
+import rospy
+from geometry_msgs.msg import PoseStamped
+
 
 class TestMainRobot1(unittest.TestCase):
     def setUp(self):
-        self.sample = type('Sample', (object,), {'payload': b'start exploration'})
+        self.sample = type('Sample', (object,), {
+                           'payload': b'test'})
 
-    # Test de start_listener et que le booléen soit bien modifié
     def test_start_listener(self):
         with patch('builtins.print') as mocked_print:
             main_robot1.start_listener(self.sample)
-            mocked_print.assert_called_with('start exploration')
+            mocked_print.assert_called_with('test')
             self.assertTrue(main_robot1.start_exploration)
 
-    # Test de finish_listener et que le booléen soit bien modifié
     def test_finish_listener(self):
         with patch('builtins.print') as mocked_print:
             with patch.object(main_robot1.launch_exploration, 'shutdown') as mocked_shutdown:
                 main_robot1.finish_listener(self.sample)
-                mocked_print.assert_called_with('start exploration')
+                mocked_print.assert_called_with('test')
                 mocked_shutdown.assert_called_once()
                 self.assertFalse(main_robot1.exploration_running)
-    
-    # Test que la fonction main() change bien la valeur des booléens lorsqu'ils sont prêts à démarrer l'exploration
-    @patch('main_robot1.zenoh.open')
-    def test_main(self, mocked_open):
-        main_robot1.start_exploration = True
-        main_robot1.exploration_running = False
 
-        with patch('main_robot1.time.sleep') as mocked_sleep:
-            def side_effect(*args, **kwargs):
-                if mocked_sleep.call_count >= 2:
-                    raise StopIteration
-                return None
-            mocked_sleep.side_effect = side_effect
+    def test_identify_listener(self):
+        with patch('builtins.print') as mocked_print:
+            main_robot1.identify_listener(self.sample)
+            mocked_print.assert_called_with('test')
 
-            with patch.object(main_robot1.launch_exploration, 'start') as mocked_start:
-                with self.assertRaises(StopIteration):
-                    main_robot1.main()
-                mocked_open.assert_called_once()
-                mocked_sleep.assert_called_with(1)
-                self.assertFalse(main_robot1.start_exploration)
-                self.assertTrue(main_robot1.exploration_running)
-                mocked_start.assert_called_once()
+    def test_odom_callback(self):
+        data = MagicMock()
+        main_robot1.odom_callback(data)
+        self.assertEqual(main_robot1.last_position, data.pose.pose.position)
+
+    def test_scan_callback(self):
+        data = MagicMock()
+        main_robot1.scan_callback(data)
+        self.assertEqual(main_robot1.last_scan, data)
+
+    def test_map_callback(self):
+        data = MagicMock()
+        with patch("main_robot1.map_generation_limo") as mocked_map_gen:
+            png_mock = MagicMock()
+            png_mock.tobytes.return_value = b"mocked_png"
+            mocked_map_gen.return_value = png_mock
+            with patch.object(main_robot1.session, "declare_publisher") as mocked_declare_publisher:
+                main_robot1.map_callback(data)
+                mocked_map_gen.assert_called_once_with(
+                    data, "map", main_robot1.tfBuffer, main_robot1.cognifly_exists, True
+                )
+                mocked_declare_publisher.assert_called_once_with("map_image")
+                main_robot1.session.declare_publisher().put.assert_called_once_with(b"mocked_png")
+
+    def test_return_home_listener(self):
+        with patch('builtins.print') as mocked_print:
+            with patch.object(main_robot1, 'PoseStamped', return_value=PoseStamped()) as mocked_pose_stamped:
+                with patch.object(rospy, 'Publisher', autospec=True) as mocked_publisher:
+                    main_robot1.return_home_listener(self.sample)
+                    mocked_print.assert_called_with('test')
+                    mocked_publisher.return_value.publish.assert_called_once()
+                    args, kwargs = mocked_publisher.return_value.publish.call_args
+                    sent_msg = args[0]
+                    self.assertIsInstance(sent_msg, PoseStamped)
+
+    def test_receive_existence(self):
+        main_robot1.cognifly_exists = False
+        main_robot1.receive_existence(self.sample)
+        self.assertTrue(main_robot1.cognifly_exists)
+
+    def test_send_existence(self):
+        with patch.object(main_robot1.session, "declare_publisher") as mocked_declare_publisher:
+            main_robot1.send_existence()
+            mocked_declare_publisher.assert_called_once_with("limo_exists")
+            main_robot1.session.declare_publisher().put.assert_called_once_with(True)
+
 
 if __name__ == '__main__':
     unittest.main()
--- sim/main_robot2.test.py
+++ sim/main_robot2.test.py
@@ -1,31 +1,38 @@
 import unittest
 from unittest.mock import MagicMock, patch, call
 import main_robot2 as mr2
-from nav_msgs.msg import Odometry
+from nav_msgs.msg import Odometry, OccupancyGrid
+import warnings
 
 
 class TestMainRobot2(unittest.TestCase):
-    def test_rotation_left(self):
+    @patch("main_robot2.rospy.sleep")
+    def test_rotation_left(self, mock_sleep):
         with patch("main_robot2.pub") as mock_pub:
             mr2.rotation_left()
             mock_pub.publish.assert_called()
 
-    def test_rotation_right(self):
+    @patch("main_robot2.rospy.sleep")
+    def test_rotation_right(self, mock_sleep):
         with patch("main_robot2.pub") as mock_pub:
             mr2.rotation_right()
             mock_pub.publish.assert_called()
 
-    def test_forward(self):
+    @patch("main_robot2.rospy.sleep")
+    def test_forward(self, mock_sleep):
         with patch("main_robot2.pub") as mock_pub:
             mr2.forward()
             mock_pub.publish.assert_called()
 
-    def test_backward(self):
+    @patch("main_robot2.rospy.sleep")
+    def test_backward(self, mock_sleep):
+        warnings.filterwarnings("ignore")
         with patch("main_robot2.pub") as mock_pub:
             mr2.backward()
             mock_pub.publish.assert_called()
 
-    def test_lateral_movement(self):
+    @patch("main_robot2.rospy.sleep")
+    def test_lateral_movement(self, mock_sleep):
         with patch("main_robot2.pub") as mock_pub:
             mr2.lateral_movement("up")
             mock_pub.publish.assert_called()
@@ -33,7 +40,8 @@ class TestMainRobot2(unittest.TestCase):
             mr2.lateral_movement("down")
             mock_pub.publish.assert_called()
 
-    def test_back_to_base(self):
+    @patch("main_robot2.rospy.sleep")
+    def test_back_to_base(self, mock_sleep):
         with patch("main_robot2.pub") as mock_pub, patch("main_robot2.lateral_movement") as mock_lateral_movement:
             mr2.line_counter = 1
             mr2.forward_counter = 2
@@ -42,7 +50,8 @@ class TestMainRobot2(unittest.TestCase):
             mock_lateral_movement.assert_called()
             mock_pub.publish.assert_called()
 
-    def test_drone_movement_forward(self):
+    @patch("main_robot2.rospy.sleep")
+    def test_drone_movement_forward(self, mock_sleep):
         with patch("main_robot2.forward") as mock_forward, patch("main_robot2.back_to_base") as mock_back_to_base, patch("main_robot2.stop_event") as mock_stop_event, patch("main_robot2.lateral_movement") as mock_lateral_movement:
             mr2.line_counter = 0
             # Configurez stop_event.is_set pour retourner False
@@ -53,7 +62,8 @@ class TestMainRobot2(unittest.TestCase):
             mock_lateral_movement.assert_called_with("up")
             mock_back_to_base.assert_called()
 
-    def test_drone_movement_backward(self):
+    @patch("main_robot2.rospy.sleep")
+    def test_drone_movement_backward(self, mock_sleep):
         with patch("main_robot2.backward") as mock_backward, patch("main_robot2.back_to_base") as mock_back_to_base, patch("main_robot2.stop_event") as mock_stop_event, patch("main_robot2.lateral_movement") as mock_lateral_movement:
             mr2.line_counter = 1
             # Configurez stop_event.is_set pour retourner False
@@ -64,7 +74,8 @@ class TestMainRobot2(unittest.TestCase):
             mock_lateral_movement.assert_called_with("up")
             mock_back_to_base.assert_called()
 
-    def test_drone_movement_stop_event(self):
+    @patch("main_robot2.rospy.sleep")
+    def test_drone_movement_stop_event(self, mock_sleep):
         with patch("main_robot2.lateral_movement") as mock_lateral_movement, patch("main_robot2.back_to_base") as mock_back_to_base, patch("main_robot2.stop_event") as mock_stop_event:
             mr2.line_counter = 0
 
@@ -129,34 +140,64 @@ class TestMainRobot2(unittest.TestCase):
             mr2.return_home_listener(sample)
             mock_back_to_base.assert_called()
 
+    def mock_sleep(time):
+        pass
+
+    @patch("main_robot2.rospy.sleep", mock_sleep)
+    @patch("main_robot2.time.sleep", mock_sleep)
     @patch("main_robot2.session.declare_subscriber")
     @patch("main_robot2.session.declare_publisher")
     @patch("main_robot2.rospy.Subscriber")
-    @patch("main_robot2.pub.publish")
-    def test_main(self, mock_publish, mock_subscriber, mock_declare_publisher, mock_declare_subscriber):
-        with self.assertRaises(InterruptedError):
-            mr2.main()
+    def test_main(self, mock_subscriber, mock_declare_publisher, mock_declare_subscriber):
+        warnings.filterwarnings("ignore")
+        # Limiter le nombre d'itérations dans la boucle while
+        iteration_limit = 3
+        iteration_counter = [0]
 
-        # Vérifie que les subscribers ont été déclarés
-        mock_declare_subscriber.assert_has_calls([
-            call('start_drone', mr2.start_listener),
-            call('identify', mr2.identify_listener),
-            call('finish', mr2.finish_listener),
-            call('return_home', mr2.return_home_listener)
-        ])
+        # Remplacer la fonction 'time.sleep' par une fonction qui compte les itérations
+        def count_iterations(time):
+            iteration_counter[0] += 1
+            if iteration_counter[0] >= iteration_limit:
+                raise KeyboardInterrupt
 
-        # Vérifie que les publishers ont été déclarés
-        mock_declare_publisher.assert_has_calls([
-            call('drone_state'),
-            call('logger')
-        ])
+        with patch("main_robot2.time.sleep", side_effect=count_iterations):
+            with self.assertRaises(KeyboardInterrupt):
+                mr2.main()
 
-        # Vérifie que le message initial a été publié
-        mock_publish.assert_called_with(mr2.move)
+            # Vérifiez que les fonctions sont appelées avec les bons arguments
+            mock_declare_subscriber.assert_has_calls([
+                call('start_drone', mr2.start_listener),
+                call('identify', mr2.identify_listener),
+                call('finish', mr2.finish_listener),
+                call('return_home', mr2.return_home_listener)
+            ])
+            mock_declare_publisher.assert_has_calls([
+                call('drone_state'),
+                call('logger')
+            ])
+            mock_subscriber.assert_has_calls([
+                call("/robot2/odom", Odometry, mr2.odom_callback),
+                call('/robot2/map', OccupancyGrid, mr2.map_callback)
+            ])
 
-        # Vérifie que rospy.Subscriber a été appelé avec les bons arguments
-        mock_subscriber.assert_called_with(
-            "/robot2/odom", Odometry, mr2.odom_callback)
+    def test_receive_existence(self):
+        sample = MagicMock()
+        with patch("main_robot2.send_existence") as mock_send_existence:
+            mr2.limo_exists = False
+            mr2.receive_existence(sample)
+            self.assertTrue(mr2.limo_exists)
+            mock_send_existence.assert_called()
+
+            mr2.limo_exists = True
+            mr2.receive_existence(sample)
+            mock_send_existence.assert_called_once()
+
+    def test_send_existence(self):
+        with patch("main_robot2.session") as mock_session:
+            mock_publisher = MagicMock()
+            mock_session.declare_publisher.return_value = mock_publisher
+            mr2.send_existence()
+            mock_publisher.put.assert_called_with(True)
 
 
 if __name__ == "__main__":
