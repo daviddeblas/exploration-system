6931c7eb6ec9ae483ffdc9bb3de389a4324f0b29
daviddeblas <71023594+daviddeblas@users.noreply.github.com>
2023-04-18 19:22:32 -0400

    Correction erreur dans les tests de Mission.vu et rajout de test

--- groundstation/website/src/views/__tests__/Mission.spec.ts
+++ groundstation/website/src/views/__tests__/Mission.spec.ts
@@ -1,6 +1,6 @@
 import { mount } from "@vue/test-utils";
 import Missions from "../Missions.vue";
-import { describe, it, expect, beforeEach, vi } from "vitest";
+import { describe, it, expect, beforeEach, afterEach, vi } from "vitest";
 import { socketProvider } from "@/plugins/socket";
 import { SocketTestHelper } from "@/helper/socket-test-helper";
 import { SERVER_URL } from "@/common/constants";
@@ -8,11 +8,41 @@ import { SERVER_URL } from "@/common/constants";
 describe("Missions", () => {
   let wrapper: any;
   let socketTestHelper: any;
+
   beforeEach(() => {
     socketTestHelper = new SocketTestHelper();
     wrapper = mount(Missions, {
       global: { provide: { [socketProvider as symbol]: socketTestHelper } },
     });
+  });
+
+  afterEach(() => {
+    socketTestHelper.disconnect();
+  });
+
+  it("renders missions list correctly", async () => {
+    const mission = {
+      id: "1",
+      start: "2023-04-18T00:00:00",
+      end: "2023-04-18T01:30:45",
+      has_rover: true,
+      has_drone: false,
+      distance_rover: 0,
+      distance_drone: 0,
+      is_sim: false,
+    };
+
+    wrapper.vm.missions = [mission];
+    await wrapper.vm.$nextTick();
+
+    const missionElement = wrapper.get("table");
+    expect(missionElement.text()).toContain("Début");
+    expect(missionElement.text()).toContain("Durée");
+    expect(missionElement.text()).toContain("Robots");
+    expect(missionElement.text()).toContain("Rover");
+    expect(missionElement.text()).toContain("Distance parcourue (rover)");
+    expect(missionElement.text()).not.toContain("Distance parcourue (drone)");
+    expect(missionElement.text()).toContain("Simulation");
   });
 
   it("updates missions list when a mission update is received", () => {
@@ -56,7 +86,7 @@ describe("Missions", () => {
       distance_drone: 0,
       is_sim: false,
     };
-  
+
     const duration = wrapper.vm.missionDuration(mission);
     expect(duration).toBe("90m45s");
   });
